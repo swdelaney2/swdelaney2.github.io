1e2094258272f6cb1a410ada4f4486cf7302c0b7
Stephen Delaney <swdelaney2@gmail.com>
2015-11-09 16:54:54 -0600

    added listener for space button

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,3 +1,7 @@
+$ ("#playspace").hide();
+$ ("#scores").hide();
+
+
 $(document).ready(function(){ // start
 
 function getNames () {
@@ -5,13 +9,29 @@ function getNames () {
    var secondPlayer = $('#playerTwoName').val();
  console.log('The first player is ' + firstPlayer);
   console.log('The second player is ' + secondPlayer);
+  $ ("#nameForm").hide();
+  $ ("#playspace").fadeIn();
+  $ ("#scores").fadeIn();
+  $( "#scores" ).append(firstPlayer + ': ' + firstPlayerScore + '   (Player One)<p>' + secondPlayer + ': ' + secondPlayerScore + '   (Player Two)');
+    $( "#playspace" ).append('Welcome to the trivia game! This game contains ten questions. Player one, if you know the answer, buzz in by typing "Q." Player two, if you know the answer, buzz in by typing "P." <p>Got it? Great! Press "SPACE" to begin.');
+    window.addEventListener('keyup', function(event) {
+  // look for specific keys to be pressed
+  if (event.keyCode == 32) {
+    console.log('Space button pressed.');
+  }
+});
 };
-
 
 $("#submitbutton").click(function() {
 getNames();
-$ ("#nameForm").hide();
 });
 
 
+
+
+
+
 }); // end
+
+var firstPlayerScore = 0;
+var secondPlayerScore = 0;
