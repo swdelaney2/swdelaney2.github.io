01c884e104fea9477330266b4d32856ca950734e
Stephen Delaney <swdelaney2@gmail.com>
2015-11-10 11:33:07 -0600

    added initial question and answer functionality

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -13,11 +13,13 @@ function getNames () {
   $ ("#playspace").fadeIn();
   $ ("#scores").fadeIn();
   $( "#scores" ).append(firstPlayer + ': ' + firstPlayerScore + '   (Player One)<p>' + secondPlayer + ': ' + secondPlayerScore + '   (Player Two)');
-    $( "#playspace" ).append('Welcome to the trivia game! This game contains ten questions. Player one, if you know the answer, buzz in by typing "Q." Player two, if you know the answer, buzz in by typing "P." <p>Got it? Great! Press "SPACE" to begin.');
+$('#playspace').append('<div id="directions"></div>');
+    $( "#directions" ).append('Welcome to the trivia game! This game contains ten questions. Player one, if you know the answer, buzz in by typing "Q." Player two, if you know the answer, buzz in by typing "P." <p>Got it? Great! Press "SPACE" to begin.');
     window.addEventListener('keyup', function(event) {
   // look for specific keys to be pressed
   if (event.keyCode == 32) {
-    console.log('Space button pressed.');
+    $( "#directions" ).hide();
+startRound();
   }
 });
 };
@@ -27,11 +29,31 @@ getNames();
 });
 
 
-
-
-
-
 }); // end
 
+function startRound(){
+  $('#playspace').append(round);
+  $('#round').append(questionsAndAnswers.QuestionOne[0]);
+  console.log('Space button pressed.');
+  window.addEventListener('keyup', function(event) {
+
+  if (event.keyCode == 81) { //Q, player one
+    console.log('Player one has keyed in.');
+  $('#round').append('<form><div><label for="playerOneRound">Player 1 Answer:</label><input type="text" id="playerOneRound" /></div></form><p><div class="button"><button type="button" id="submitbutton">Submit</button></div>');
+  }
+  if (event.keyCode == 80) { //P, player two
+  $('#round').append('<form><div><label for="playerTwoRound">Player 2 Answer:</label><input type="text" id="playerTwoRound" /></div></form><p><div class="button"><button type="button" id="submitbutton">Submit</button></div>');
+};
+});
+
+};
+function checkAnswer (){};
+
+var round = $('<div id=round>');
 var firstPlayerScore = 0;
 var secondPlayerScore = 0;
+var questionsAndAnswers = {
+  QuestionOne: ['Who is the current president?', 'Barack Obama'],
+  QuestionTwo: ['What is my favorite color?', 'Green'],
+
+}
