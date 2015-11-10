f024f493bed20b6556790117116a222e53d9d8ca
Stephen Delaney <swdelaney2@gmail.com>
2015-11-10 16:18:20 -0600

    updating score works for player one

--- project1/scripts/app.js
+++ project1/scripts/app.js
@@ -1,6 +1,7 @@
 $ ("#playspace").hide();
 $ ("#scores").hide();
 
+
 var directionsListener = function(event) {
   if (event.keyCode == 32) {
     $( "#directions" ).hide();
@@ -19,7 +20,11 @@ function getNames () {
   $ ("#nameForm").hide();
   $ ("#playspace").fadeIn();
   $ ("#scores").fadeIn();
-  $( "#scores" ).append(firstPlayer + ': ' + firstPlayerScore + '   (Player One)<p>' + secondPlayer + ': ' + secondPlayerScore + '   (Player Two)');
+
+  $( "#scores" ).append(firstPlayer + "(Player One): <div id='namedOneScore'>" + 0 + "</div><br>" + secondPlayer + "(Player Two): <div id='namedTwoScore'>" + 0 + "</div>");
+
+
+
 $('#playspace').append('<div id="directions"></div>');
     $( "#directions" ).append('Welcome to the trivia game! This game contains ten questions. Player one, if you know the answer, buzz in by typing "Q." Player two, if you know the answer, buzz in by typing "P." <p>Got it? Great! Press "SPACE" to begin.');
 
@@ -53,28 +58,16 @@ function startRound(){
 
   if (event.keyCode == 81) { //Q, player one
     console.log('Player one has keyed in.');
-turnPlayerOne(playerOneAll);
-  }
+turnForEachPlayer(playerOneAll);
+  };
   if (event.keyCode == 80) { //P, player two
-  $('#round').append('<form id="formround"><div><label for="playerTwoRound">Player 2 Answer:</label><input type="text" id="playerTwoRound"/></div></form><p><div class="button"><button type="button" id="submitbuttonRoundPlayerTwo">Submit</button></div>');
-  $("#submitbuttonRoundPlayerTwo").click(function() {
-  checkAnswerPlayerTwo();
-  });
+    turnForEachPlayer(playerTwoAll);
 };
 });
 
 }; // end of start round
 
-// WORK AREA
-// var myFuncName = 'somethingWicked';
-// function myFuncName(){console.log('wicked');};
-// somethingWicked(); // consoles 'wicked'
 
-// want to do above
-// var funcs = {};
-// var myFuncName = 'somethingWicked';
-// funcs[myFuncName] = function(){console.log('wicked');};
-// funcs.somethingWicked(); // consoles 'wicked'
 
 var playerOneAll = {
   label: "Player 1 Answer:",
@@ -83,43 +76,60 @@ var playerOneAll = {
   callSubmitId: "#submitbuttonRoundPlayerOne",
   checkAnswer: 'checkAnswerPlayerOne()',
 }
-// END WORK AREA
+
+var playerTwoAll = {
+  label: "Player 2 Answer:",
+  id: "playerTwoRound",
+  submitId: "submitbuttonRoundPlayerTwo",
+  callSubmitId: "#submitbuttonRoundPlayerTwo",
+  checkAnswer: 'checkAnswerPlayerTwo()',
+}
 
 
-function turnPlayerOne(whichPlayer){
-  $('#round').append('<form id="formround"><div><label for=' + whichPlayer.id + '>' + whichPlayer.label + '</label><input type="text" id=' + whichPlayer.id + '></div></form><p><div class="button"><button type="button" id=' + whichPlayer.submitId + '>Submit</button></div>');
+
+function turnForEachPlayer(whichPlayer){
+  $('#round').append('<form id="formround"><div><label for=' + whichPlayer.id + '>' + whichPlayer.label + '</label><input type="text" id=' + whichPlayer.id + '></div><p><div class="button"><button type="button" id=' + whichPlayer.submitId + '>Submit</button></div></form>');
   $(whichPlayer.callSubmitId).click(function() {
     console.log('submitbuttonRoundPlayerOne has been clicked');
   eval(whichPlayer.checkAnswer);
   });
 }
 
-
-
-// function turnPlayerOne(){
-//   $('#round').append('<form id="formround"><div><label for="playerOneRound">Player 1 Answer:</label><input type="text" id="playerOneRound"/></div></form><p><div class="button"><button type="button" id="submitbuttonRoundPlayerOne">Submit</button></div>');
-//   $("#submitbuttonRoundPlayerOne").click(function() {
-//     console.log('submitbuttonRoundPlayerOne has been clicked');
-//   checkAnswerPlayerOne();
-//   });
-// }
-
 function checkAnswerPlayerOne (){ // need to make for player two
-  console.log('checkAnswerPlayerOne has been called')
+  // console.log('checkAnswerPlayerOne has been called');
   $ ("#formround").hide();
   var firstPlayerAnswer = $('#playerOneRound').val();
 if (firstPlayerAnswer == questionsAndAnswers.QuestionOne[1]) {
-console.log('Player one should get a point here. Correct answer was ' + questionsAndAnswers.QuestionOne[1])};
+    $('#round').append('<p>You got it! One point for player one!');
+onePointForPlayerOne();
+} else {
+  $('#round').append('<p>Sorry, that is not correct. Player two, you now have a chance to answer.');
+  turnForEachPlayer(playerTwoAll);
+}
 };
 
 
 
-
-
 var round = $('<div id=round>');
-var firstPlayerScore = 0;
+// var firstPlayerScore = 0;
 var secondPlayerScore = 0;
 var questionsAndAnswers = {
   QuestionOne: ['Who is the current president?', 'Barack Obama'],
   QuestionTwo: ['What is my favorite color?', 'Green'],
+}
+
+
+
+
+// SCORING BELOW
+
+function baseScoreOne() {
+  this.scoreOne = 0;
+};
+
+var firstPlayerScore = new baseScoreOne();
+
+function onePointForPlayerOne() {
+  firstPlayerScore.scoreOne += 1;
+  $('#namedOneScore').html(firstPlayerScore.scoreOne);
 }
